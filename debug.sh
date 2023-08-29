5685c77de36cdad9bb412156d567cf514bd89d8f
efratmaya <efratmaya111@gmail.com>
2023-08-29 12:37:47 +0300

    part 2 of best practice

--- debug.sh
+++ debug.sh
@@ -1,4 +1,7 @@
 docker stop $(docker ps -a -q)
-docker rmi -f chat-app
+#docker rmi -f chat-app
+docker rmi -f $(docker images -aq)
+docker rm -f $(docker ps -a -q)
 docker build -t chat-app .
-docker run -d -p 5000:5000 chat-app
+docker run --name chat_container -d -p 5000:5000 --memory=1g --memory-reservation=512m --cpus=1 --cpuset-cpus=2 chat-app 
+#docker container run --rm -it -d --name chat_container --memory=1g --memory-reservation=512m nginx:alpine
